239771c77af1bdb1bd3e6402cf6abea6dfc41a09
forseasummer <summerrohh@gmail.com>
2022-09-30 17:22:50 +1000

    PresetScreen

--- app/screens/PresetScreen.js
+++ app/screens/PresetScreen.js
@@ -1,25 +1,64 @@
-import React, { useState } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import Screen from "../components/Screen";
 
 function PresetScreen(props) {
-  const [preset, setPreset] = useState([]);
+  const [presets, setPresets] = useState([]);
+  const [keys, setKeys] = useState([]);
 
   useEffect(() => {
-    // getData()
+    getData();
   }, []);
 
   // AsyncStorage의 모든 아이템을 가져오기(작업중)
   const getData = async () => {
     try {
-      const presetData = JSON.parse(await AsyncStorage.getItem("preset16  "));
-      setPreset(presetData);
-      console.log("get data: ", presetData);
+      // AsyncStorage의 모든 키 가져오기
+      let keys = await AsyncStorage.getAllKeys();
+      console.log(keys);
+      setKeys(keys);
+      // keys.forEach((key) => console.log(key));
+      // const presetsData = JSON.parse(await AsyncStorage.getItem("preset16  "));
+      // setPresets(presetsData);
+      // console.log("get data: ", presetsData);
     } catch (error) {
       console.log(error);
     }
+  };
+
+  // soundcard들을 렌더링하는 function
+  const renderItem = ({ item }) => {
+    return (
+      <View>
+        <TouchableOpacity
+          // onPress={() => handlePress(itemMusic)}
+          style={[
+            styles.soundCard,
+            // { backgroundColor: isPlaying ? "#bebebe" : "#fff" },
+            { backgroundColor: "#fff" },
+          ]}
+        >
+          {/* <MaterialCommunityIcons
+            name={iconName}
+            size={20}
+            // color={isPlaying ? "#fff" : "#000"}
+            color={  "#000"}
+          /> */}
+          <Text style={[styles.text, { color: "#000" }]}>
+            preset
+            {/* {itemName} */}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
   };
 
   return (
@@ -31,8 +70,8 @@ function PresetScreen(props) {
           columnWrapperStyle={styles.row}
           showsVerticalScrollIndicator={false}
           showsHorizontalScrollIndicator={false}
-          data={item}
-          keyExtractor={(Item) => Item.name}
+          data={keys}
+          keyExtractor={(index) => index}
           renderItem={renderItem}
         ></FlatList>
       </View>
@@ -62,6 +101,17 @@ const styles = StyleSheet.create({
     // flex: 1,
     justifyContent: "space-between",
     marginVertical: 15,
+  },
+  soundCard: {
+    backgroundColor: "#fff",
+    width: 70,
+    height: 70,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "black",
   },
 });
 
